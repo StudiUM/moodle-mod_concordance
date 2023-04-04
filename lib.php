de2cc791bb728a1b8ee346c495ac99acbaa69ab9
Marie-Eve Lévesque <marie-eve.levesque.8@umontreal.ca>
2023-04-04 14:13:03 -0400

    EVOSTDM-2777 - Thème 4.0 - Pages et éléments spécifiques

--- lib.php
+++ lib.php
@@ -49,6 +49,8 @@ function concordance_supports($feature) {
             return true;
         case FEATURE_SHOW_DESCRIPTION:
             return true;
+        case FEATURE_MOD_PURPOSE:
+            return MOD_PURPOSE_ADMINISTRATION;
 
         default:
             return null;
